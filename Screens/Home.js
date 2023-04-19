d0a0af3ce3c9a20fb392612e98af07e3fb961578
Cornea Marc <corneamarc@gmail.com>
2023-04-19 21:48:49 +0300

    Add bottom tab navigator

---
+++ Screens/Home.js
@@ -0,0 +1,11 @@
+
+import React, { useState, useEffect } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, TouchableWithoutFeedback, Keyboard, Image, Platform } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+
+const Home = ({ }) => {
+    return (
+        <Text> Baluba </Text>
+    );
+}
+export default Home
